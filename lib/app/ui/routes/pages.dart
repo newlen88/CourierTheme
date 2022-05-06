6c30a4884b3a24447c7feb33988b4ec05107ffa7
Luis Eduardo Nuñez <new_len@hotmail.com>
2022-05-06 12:51:38 -0500

    inicial

--- lib/app/ui/routes/pages.dart
+++ lib/app/ui/routes/pages.dart
@@ -1,19 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+import '../pages/splash/splash_page.dart';
 import 'routes.dart';
 
 Map<String, Widget Function(BuildContext)> appRoutes = {
-  Routes.SPLASH: (_) => const SplashPage(),
-  Routes.SETUP: (_) => const SetupPage(),
-  Routes.ONBOARD: (_) => const OnboardPage(),
-  Routes.LOGIN: (_) => const LoginPage(),
-  Routes.MAIN: (_) => const MenuPage(),
-
-  /* --------------------------------- MODULOS -------------------------------- */
-  Routes.CATALOGO: (_) => const CatalogoPage(),
-  Routes.CLIENTES: (_) => const ClientesPage(),
-  Routes.GASTOS: (_) => const GastosPage(),
-  // Routes.MESAS: (_) => const CestaPage(),
-  Routes.TOMA_FISICA: (_) => const TomaFisicaPage(),
-
-  // Routes.PEDIDOS: (_) => ProductosPage(),
-  // Routes.CESTA: (_) => const CestaPage(),
+  Routes.rSplash: (_) => const SplashPage(),
 };
